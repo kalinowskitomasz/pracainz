454ad5bd62accc04a58bd8cc805b8eff71eb4a4b
kalinowskitomasz <tomasz.kalinow@gmail.com>
2016-06-25 05:06:41 +0200

    poprawione bledy, rozdzielone uruchamianie client, server

--- server.py
+++ server.py
@@ -12,13 +12,22 @@ URG = 0x20
 ECE = 0x40
 CWR = 0x80
 
+serverPort = "9000"
+
 class Server:
-	def synAck(pkt):
+	def synAck(self,pkt):
 		F = pkt['TCP'].flags
 		if F & SYN:
 			tcpSynAck = TCP(dport = 12345, sport = 80, flags = "SA", ack = 0,seq = 1 )
-			send(IP(dst = '192.168.1.162')/tcpSynAck1)
+			send(IP(dst = "192.168.1.162")/tcpSynAck)
 
 	def waitForConnection(self):
-		sniff(iface="eth0", prn=synAck, store=0)
-		#tcp2 = TCP(dport = 12345, sport = 80, flags = "SA", ack = 0,seq = 1 )
+		sniff(iface="eth0", prn=self.synAck, store=0, filter = "tcp and port "+serverPort)
+
+
+if __name__ == "__main__":
+	try:
+		server = Server()
+		server.waitForConnection()
+	except Exception as e:
+		print e
