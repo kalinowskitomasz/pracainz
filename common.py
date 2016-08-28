4929ba45479ebbe4ccd20dc2b059fb8f5ecf38ca
tk <tomasz.kalinow@gmail.com>
2016-08-28 18:18:55 +0200

    handshake dziala

--- common.py
+++ common.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from scapy.all import *
 import random
+from abc import ABCMeta, abstractmethod
 
 FIN = 0x01
 SYN = 0x02
@@ -20,4 +21,34 @@ def generate_data(n):
 
 def enum(**enums):
 	return type('Enum', (), enums)
+
+States = enum(LISTENING=0, SYN_SENT=1, SYN_RECEIVED=2, ESTABLISHED=3)
+
+class Observable(object):
+	def __init__(self):
+		self.observers = []
+
+	def register(self, observer):
+		if not observer in self.observers:
+			self.observers.append(observer)
+
+	def unregister(self, observer):
+		if observer in self.observers:
+			self.observers.remove(observer)
+
+	def unregister_all(self):
+		if self.observers:
+			del self.observers[:]
+
+	def update_observers(self, *args, **kwargs):
+		for observer in self.observers:
+			observer.update(*args, **kwargs)
+
+
+class Observer(object):
+	__metaclass__ = ABCMeta
+
+	@abstractmethod
+	def update(self, *args, **kwargs):
+		pass
 
--- server.py
+++ server.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
+
 from common import *
 from scapy.all import *
 
+
 interface = "en0"  # "en0"
 server_port = "9000"
-states = enum(NOT_CONNECTED=0, CONNECTING=1, CONNECTED=2, CONNECTION_ENDED=3)
+States = enum(LISTENING=0, SYN_SENT=1, SYN_RECEIVED=2, ESTABLISHED=3)
 
 
 class ConnectionManager:
 
 	def __init__(self):
-		self.sockets = []
+		self.connections = []
 
 	#############################################################
 
@@ -20,19 +22,36 @@ class ConnectionManager:
 	#############################################################
 
 	def on_socket_connected(self):
-		self.__start_new_listening_socket()
+		self.__start_new_listening_connection()
 
 	#############################################################
 
 	def	__start_new_listening_socket(self):
-		self.sockets.append(Socket())
-		self.sockets[-1]()
+		self.connections.append(Connection(self))
+		self.connecitons[-1]()
 
 	#############################################################
 
 	def __start_listening(self):
-		self.sockets.append(Socket())
-		self.sockets[0]()
+		self.connections.append(Connection(self))
+		self.connections[0]()
+
+#############################################################
+
+
+class Connection:
+
+	def __init__(self, connection_manager):
+		self.connection_manager = connection_manager
+		self.socket = Socket(self)
+
+	def __call__(self):
+		self.socket()
+
+	def set_filter(self, new_filter):
+		self.socket.filter = new_filter
+
+#############################################################
 
 
 class Socket(AnsweringMachine):
@@ -42,9 +61,10 @@ class Socket(AnsweringMachine):
 
 	#############################################################
 
-	def __init__(self, **kargs):
-		AnsweringMachine.__init__(self, filter="tcp port 9000")
-		self.state = states.NOT_CONNECTED
+	def __init__(self, connection):
+		AnsweringMachine.__init__(self, filter="tcp dst port 9000 and tcp[tcpflags] & (tcp-syn) != 0 ")
+		self.connection = connection
+		self.state = States.LISTENING
 		self.client_port = None
 		self.client_ip = None
 
@@ -65,123 +85,39 @@ class Socket(AnsweringMachine):
 	#############################################################
 
 	def is_request(self, req):
-		if self.state == states.NOT_CONNECTED:
-			flag = req['TCP'].flags
-			if flag & SYN:
-				req[TCP].dport = self.client_port
-				req[IP].src = self.client_ip
-				return True
+		if self.state == States.LISTENING:
+			self.client_port = req[TCP].sport
+			self.client_ip = req[IP].src
 
-		if self.state == states.CONNECTING:
-			if self.state == states.NOT_CONNECTED:
-				flag = req['TCP'].flags
-				if flag & ACK:
-					return True;
-
-		if self.state == states.CONNECTED:
+			self.state = States.SYN_RECEIVED
+			self.connection.set_filter("tcp dst port 9000 and tcp[tcpflags] & (tcp-syn) != 0 ")
 			return True
 
+		if self.state == States.SYN_RECEIVED:
+			self.filter = States.ESTABLISHED
+			self.connection.set_filter("tcp dst port 9000 and tcp src port %d" % self.client_port)
+			print "Connection Established"
+			return False
+
 	#############################################################
-
-
 
 	def make_reply(self, req):
-		ans = IP(src=req[IP].dst, dst=req[IP].src) / TCP(
-			flags='SA',
-			sport=req[TCP].dport,
-			dport=req[TCP].sport,
-			seq=0,
-			ack=req[TCP].seq + 1,
-		)
-		return ans
+		if self.state == States.SYN_RECEIVED:
+			ans = IP(src=req['IP'].dst, dst=req['IP'].src) / TCP(
+				flags='SA',
+				sport=req['TCP'].dport,
+				dport=req['TCP'].sport,
+				seq=0,
+				ack=req['TCP'].seq + 1,
+			)
+			self.state == States.SYN_RECEIVED
+			return ans
 
-
-class Server3(AnsweringMachine):
-
-	function_name = "server"
-	filter = "tcp port " + server_port
-
-	def __init__(self):
-		self.is_connected = False
-
-	# def send_syn_ack(self, pkt):
-	# 	ans = IP(src=pkt[IP].dst, dst=pkt[IP].src) / TCP(
-	# 		flags='SA',
-	# 		sport=pkt[TCP].dport,
-	# 		dport=pkt[TCP].sport,
-	# 		seq=0,
-	# 		ack=pkt[TCP].seq + 1,
-	# 	)
-	# 	return ans
-
-	def make_reply(self, req):
-		#if not self.is_connected:
-			#ans = self.send_syn_ack(req)
-		ans = IP(src=req[IP].dst, dst=req[IP].src) / TCP(
-			flags='SA',
-			sport=req[TCP].dport,
-			dport=req[TCP].sport,
-			seq=0,
-			ack=req[TCP].seq + 1,
-		)
-		return ans
-
-
-
-class Server2:
-
-	def __init__(self):
-		self.seq = 0
-		self.ack = 0
-		self.is_connected = False
-		self.client_ip = None
-		self.client_port = 12345
-
-	#############################################################
-
-	def __send_syn_ack(self):
-		print "send syn ack"
-
-
-	#############################################################
-
-	def __on_packet_received(self, received_packet):
-		print "aaaa"
-		received_packet.show()
-		flag = received_packet['TCP'].flags
-		if flag & SYN:
-			self.client_ip = received_packet['IP'].src
-			self.client_port = received_packet['IP'].sport
-			self.ack = received_packet['TCP'].ack
-			#self.seq = received_packet['TCP'].data.length + 1
-			#tcp_syn_ack = TCP(dport=self.client_port, sport=server_port, flags="SA", ack=self.seq, seq=self.ack)
-			send(IP(dst=self.client_ip))
-
-	#############################################################
-
-	def wait_for_connection(self):
-		sniff(iface=interface, prn=self.__on_packet_received, store=0, filter="tcp dst port 9000 ")
-
-	#############################################################
-
-	def rst(self, p):
-		ans = IP(src=p[IP].dst, dst=p[IP].src) / TCP(
-			flags='RA',
-			sport=p[TCP].dport,
-			dport=p[TCP].sport,
-			seq=0,
-			ack=p[TCP].seq + 1,
-		)
-		send(ans, verbose=False)
-		#return "%s\n => %s" % (p[IP].summary(), ans.summary())
-
-	def foo(self):
-		sniff(iface="en0", filter="tcp and tcp[tcpflags] & tcp-syn == tcp-syn", prn=self.rst)
 
 if __name__ == "__main__":
-	try:
-		comm_manager = ConnectionManager()
-		comm_manager()
+	#try:
+	comm_manager = ConnectionManager()
+	comm_manager()
 
-	except Exception as e:
-		print(e)
+	#except Exception as e:
+#		print(e)
